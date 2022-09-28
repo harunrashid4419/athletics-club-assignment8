4dc69f9ae4a31306466b4dd741934988eedc4d5a
harunorrashid4419 <harunrashid4419@gmail.com>
2022-09-28 22:32:18 +0600

    athlet container design complate

--- src/components/Athlet/Athlet.js
+++ src/components/Athlet/Athlet.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Athlet.css';
 
 const Athlet = ({athlet}) => {
     const {img, name, age, description, time} = athlet;
     return (
-        <div>
-            <h2>{athlet.name}</h2>
+        <div className='single-athlet'>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <p>For Age: {age}</p>
+            <p>For required {time}</p>
+            <button>Add To list</button>
         </div>
     );
 };
